74bc207ddb8fd0410d6591f111d1f604f612b8d8
Martha Bearskin <mbearskin@r01mw4k3.device.mst.edu>
2013-02-04 12:48:04 -0600

    Initial Commit

---
+++ Hair Height/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Hair Height
+//
+//  Created by Martha Bearskin on 2/4/13.
+//  Copyright (c) 2013 Martha Bearskin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
